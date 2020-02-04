2477c09a3b1dd6aa5f8e13b32be6a5dfbfb3b15f
ndellamaria <nrdell3@gmail.com>
2020-02-04 19:05:46 +0200

    update constants text

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -7,17 +7,17 @@ angular.module('reg')
     .constant('DASHBOARD', {
         UNVERIFIED: 'You should have received an email asking you verify your email. Click the link in the email and you can start your application!',
         INCOMPLETE_TITLE: 'You still need to complete your application!',
-        INCOMPLETE: 'If you do not complete your application before the [APP_DEADLINE], you will not be considered for the admissions lottery!',
+        INCOMPLETE: 'If you do not complete your application before [APP_DEADLINE], you will not be considered for admission to BothoHacks 2020!',
         SUBMITTED_TITLE: 'Your application has been submitted!',
-        SUBMITTED: 'Feel free to edit it at any time. However, once registration is closed, you will not be able to edit it any further.\nAdmissions will be determined by a random lottery. Please make sure your information is accurate before registration is closed!',
-        CLOSED_AND_INCOMPLETE_TITLE: 'Unfortunately, registration has closed, and the lottery process has begun.',
-        CLOSED_AND_INCOMPLETE: 'Because you have not completed your profile in time, you will not be eligible for the lottery process.',
-        ADMITTED_AND_CAN_CONFIRM_TITLE: 'You must confirm by [CONFIRM_DEADLINE].',
+        SUBMITTED: 'Feel free to edit it at any time. However, once registration is closed, you will not be able to edit it any further.',
+        CLOSED_AND_INCOMPLETE_TITLE: 'Unfortunately, registration for BothoHacks 2020 has closed.',
+        CLOSED_AND_INCOMPLETE: 'Because you have not completed your profile in time, you will not be eligible to participate in BothoHacks 2020. \nFeel free to email us at info@bothohacks.org with questions or concerns.',
+        ADMITTED_AND_CAN_CONFIRM_TITLE: 'You must confirm your participation by .',
         ADMITTED_AND_CANNOT_CONFIRM_TITLE: 'Your confirmation deadline of [CONFIRM_DEADLINE] has passed.',
-        ADMITTED_AND_CANNOT_CONFIRM: 'Although you were accepted, you did not complete your confirmation in time.\nUnfortunately, this means that you will not be able to attend the event, as we must begin to accept other applicants on the waitlist.\nWe hope to see you again next year!',
-        CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until [CONFIRM_DEADLINE]',
-        DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to HackMIT 2015! :(\nMaybe next year! We hope you see you again soon.',
+        ADMITTED_AND_CANNOT_CONFIRM: 'Although you were accepted, you did not complete your confirmation in time. \nIf you would still like to participate in BothoHacks 2020, you MUST email us at info@bothohacks.org to still be considered for the event. Thanks!',
+        CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until [CONFIRM_DEADLINE].',
+        DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to BothoHacks 2020! :(\nMaybe next year! We hope you see you again soon.',
     })
     .constant('TEAM',{
-        NO_TEAM_REG_CLOSED: 'Unfortunately, it\'s too late to enter the lottery with a team.\nHowever, you can still form teams on your own before or during the event!',
+        NO_TEAM_REG_CLOSED: 'Unfortunately, it\'s too late to enter the hackathon with a team.\nHowever, you can still form teams on your own before or during the event!',
     });
